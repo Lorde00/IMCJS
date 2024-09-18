6d8faad66ae5ba6cc677f5ea70419b2f24050d17
Lucas Orge Rodrigues <68645684+Lorde00@users.noreply.github.com>
2024-09-18 19:46:45 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,44 +1,49 @@
-
-
 let peso = 0
 let altura = 0
 let imc = 0
 
-
-alert("Bem vindo a balança de IMC! Por favor, siga as instruções abaixo.\n")
+alert("Bem vindo à balança de IMC! Por favor, siga as instruções abaixo.\n")
+nome = String(prompt("Digite seu nome: \n"))
 peso = parseFloat(prompt("Digite o seu peso em KG: \n"))
 altura = parseFloat(prompt("Digite a sua altura em metros(coloque .): \n"))
 
 imc = (peso / (altura * altura))
 
-if(imc < 16){
-    alert(`Seu IMC está em baiuxo peso muito grave, IMC = ${Math.round(imc)}`)
+if (imc < 16) {
+    alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: baixo peso muito grave`)
+} else {
+    if (imc >= 16 && imc < 17) {
+        alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: baixo peso grave`)
+    } else {
+        if (imc >= 17 && imc < 18.50) {
+            alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: baixo peso`)
+        } else {
+            if (imc >= 18.50 && imc < 25) {
+                alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: na média`)
+            } else {
+                if (imc >= 25 && imc < 30) {
+                    alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: sobrepeso`)
+                } else {
+                    if (imc >= 30 && imc < 35) {
+                        alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: obesidade grau I`)
+                    } else {
+                        if (imc >= 35 && imc < 40) {
+                            alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: obesidade grau II`)
+                        } else {
+                            if (imc >= 40) {
+                                alert(`${nome}, seu IMC entá em ${Math.round(imc)} e sua classsificação: obesidade grau III`)
+                            } else {
+                                alert("ERRO")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
-if(imc >= 16 && imc < 17) {
-    alert(`Seu IMC está em baixo peso grave, IMC = ${Math.round(imc)}`)
-}
 
-if(imc >= 17 && imc < 18.50){
-    alert(`Seu IMC está em baixo peso, IMC = ${Math.round(imc)}`)
-}
 
-if(imc >= 18.50 && imc < 25){
-    alert(`Seu IMC está na média, IMC = ${Math.round(imc)}`)
-}
 
-if(imc >= 25 && imc < 30){
-    alert(`Seu IMC está em sobrepeso, IMC = ${Math.round(imc)}`)
-}
 
-if(imc >= 30 && imc < 35){
-    alert(`Seu IMC está em obesidade grau I, IMC = ${Math.round(imc)}`)
-}
-
-if(imc >= 35 && imc < 40){
-    alert(`Seu IMC está em obesidade grau II, IMC = ${Math.round(imc)}`)
-}
-
-if(imc >= 40){
-    alert(`Seu IMC está em obesidade grau III, IMC = ${Math.round(imc)}`)
-}
